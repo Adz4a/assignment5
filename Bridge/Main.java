3a3fffc7172fa9b8b2abc1b7cbe9297933de2d82
Adz4a <82327148+Adz4a@users.noreply.github.com>
2021-11-10 22:53:39 +0600

    Add files via upload

---
+++ Bridge/Main.java
@@ -0,0 +1,19 @@
+package Bridge;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Computer computer = new PC(new WindowsOS());
+        computer.start();
+        computer.browseInternet("https://google.com");
+
+        System.out.println("-----------------------------");
+
+        Computer laptop = new Laptop(new MacOS());
+        laptop.start();
+        laptop.browseInternet("https://google.com");
+    }
+}
+
---
+++ Bridge/OperatingSystem.java
@@ -0,0 +1,6 @@
+package Bridge;
+
+public interface OperatingSystem {
+    void startup();
+    void loadUrl(String url);
+}
